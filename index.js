98cb284640df558e67bf530b606b087be559381d
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2020-01-03 23:24:51 +0530

    created randomTextGenerator() and created random numbers for US and UK

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ document.querySelector("button").addEventListener("click", function(event) {
         defaultName = document.querySelector("input").value;
     }
     console.log(defaultName);
+    randomTextGenerator();
 });
 
 function randomTextGenerator() {
-    var text = [
+    var randomNumber_US = Math.floor((Math.random() * 3) + 1);
+    var randomNumber_UK = Math.floor((Math.random() * 3) + 1);
+    window.alert(randomNumber_UK + ", " + randomNumber_US);
 
-    ]
+    var names_US = [
+        "Willy the Goblin",
+        "Big Daddy",
+        "Father Christmas",
+    ];
+    var names_UK = [
+        "the soup kitchen",
+        "Disneyland",
+        "the White House"
+    ];
 }
