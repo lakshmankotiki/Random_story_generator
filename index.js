91581473e1dcd77381cfd2f69de35ad4c467f982
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2020-01-04 00:37:11 +0530

    refactored the code for generating random text

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 document.querySelector("button").addEventListener("click", function(event) {
     // console.log(event);
-    var defaultName;
+    //random numbers generation from 1 to 3
+    var randomNumber_one = Math.floor(Math.random() * 3);
+    var randomNumber_two = Math.floor(Math.random() * 3);
+    var randomNumber_three = Math.floor(Math.random() * 3);
+
+    //raw text for concatenation
+    var rawText1 = [
+        "Willy the Goblin",
+        "Big Daddy",
+        "Father Christmas",
+    ];
+    var rawText2 = [
+        "the soup kitchen",
+        "Disneyland",
+        "the White House"
+    ];
+    var rawText3 = [
+        "spontaneously combusted",
+        "melted into a puddle on the sidewalk",
+        "turned into a slug and crawled away"
+    ];
+
+    var defaultName, concatenation;
     if (document.querySelector("input").value === "") {
         document.querySelector("input").value = "Bob";
         defaultName = document.querySelector("input").value;
     } else {
         defaultName = document.querySelector("input").value;
     }
-    console.log(defaultName);
-    randomTextGenerator();
+    concatenation = "It was 94 fahrenheit outside, so " + rawText1[randomNumber_one] +
+                    " went for a walk. When they got to " + rawText2[randomNumber_two] +
+                    ", they stared in horror for a few moments, then " + rawText3[randomNumber_three] +
+                    ". "+defaultName+ " saw the whole thing, but was not surprised — " + rawText1[randomNumber_one] + 
+                    " weighs 300 pounds, and it was a hot day.";
+    console.log(concatenation);
 });
-
-function randomTextGenerator() {
-    var randomNumber_US = Math.floor((Math.random() * 3) + 1);
-    var randomNumber_UK = Math.floor((Math.random() * 3) + 1);
-    window.alert(randomNumber_UK + ", " + randomNumber_US);
-
-    var names_US = [
-        "Willy the Goblin",
-        "Big Daddy",
-        "Father Christmas",
-    ];
-    var names_UK = [
-        "the soup kitchen",
-        "Disneyland",
-        "the White House"
-    ];
-}
