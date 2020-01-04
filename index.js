5498c747f2be56f2a462c7bc45a2927a173b06a2
lakshmankotiki <kotiki.lakshman1996@gmail.com>
2020-01-04 13:17:21 +0530

    added different temprature based on country condition based

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 document.querySelector("button").addEventListener("click", function(event) {
-    // console.log(event);
     //random numbers generation from 1 to 3
-    var randomNumber_one = Math.floor(Math.random() * 3);
-    var randomNumber_two = Math.floor(Math.random() * 3);
-    var randomNumber_three = Math.floor(Math.random() * 3);
+    var randomNumber_one = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    var randomNumber_two = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    var randomNumber_three = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
 
     //raw text for concatenation
     var rawText1 = [
@@ -22,17 +21,28 @@ document.querySelector("button").addEventListener("click", function(event) {
         "turned into a slug and crawled away"
     ];
 
-    var defaultName, concatenation;
+    //variable declarations
+    var defaultName, concatenation, degrees;
+
+    //adding cutom name if user not giving name else will concate th given name
     if (document.querySelector("input").value === "") {
         document.querySelector("input").value = "Bob";
         defaultName = document.querySelector("input").value;
     } else {
         defaultName = document.querySelector("input").value;
     }
-    concatenation = "It was 94 fahrenheit outside, so " + rawText1[randomNumber_one] +
+
+    //checking US radio checked or not
+    if (document.querySelectorAll("input#us")[0].checked) {
+        degrees = 94;
+    } else {
+        degrees = 34;
+    }
+    concatenation = "It was "+degrees+" fahrenheit outside, so " + rawText1[randomNumber_one] +
                     " went for a walk. When they got to " + rawText2[randomNumber_two] +
                     ", they stared in horror for a few moments, then " + rawText3[randomNumber_three] +
                     ". "+defaultName+ " saw the whole thing, but was not surprised — " + rawText1[randomNumber_one] + 
                     " weighs 300 pounds, and it was a hot day.";
-    console.log(concatenation);
+    // console.log(concatenation);
+    document.querySelector("p").textContent = concatenation;
 });
